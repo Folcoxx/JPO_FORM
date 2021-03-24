0951105f81abcf1599ac62c2af44e7b26aca4279
Folcoxx <62104391+Folcoxx@users.noreply.github.com>
2021-03-24 22:10:22 +0100

    test

--- traitement.php
+++ traitement.php
@@ -3,7 +3,7 @@
 
 try
 {
-    $bdd = new PDO('mysql:host=127.0.0.1:3306;dbname=iut;charset=utf8', 'root', 'Root1');
+    $bdd = new PDO('mysql:host=db;dbname=iut;charset=utf8', 'root', 'Root1');
 }
 catch(Exception $e)
 {
